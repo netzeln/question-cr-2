8b52d3a503858502227b9dc461ae55a8509816b1
nic <netzeln@gmail.com>
2016-04-01 15:50:20 -0700

    add a toggling feature for updating a question

--- app/components/edit-question.js
+++ app/components/edit-question.js
@@ -1,17 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  editQuestion: false,
   actions: {
-    update(question){
+    update(question, editQuestion){
       var params = {
         theQuestion: this.get('theQuestion'),
         asker: this.get('asker'),
         date: Date(),
         inContext: this.get('inContext')
       };
-
-      console.log('first function:', params);
+      console.log(editQuestion);
+      this.set('editQuestion', false);
       this.sendAction('updateAsk', question, params);
+    },
+    showUpdate(editQuestion){
+      console.log('something happned');
+      console.log();
+      if(this.editQuestion){
+        this.set('editQuestion', false);
+      }else{
+        this.set('editQuestion', true);
+      }
     }
   }
 });
--- app/components/question-display.js
+++ app/components/question-display.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
   actions:{
     deleteQuestion(question){
       if(confirm('is this question REALLY worth deletion?')){
         this.sendAction('deleteQuestion', question);
       }
     }
+
   }
 });

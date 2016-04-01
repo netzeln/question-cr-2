5b43462a402e7b7af5ae777e6333e0dddcb5ac42
nic <netzeln@gmail.com>
2016-04-01 14:01:10 -0700

    add ability to delete answers and stay on page despite errors saying something is not a function

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions:{
+    deleteAnswer(answer){
+      if(confirm('is this answer REALLY worth deletion?')){
+        
+        this.sendAction('deleteAnswer', answer);
+      }
+    }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -3,10 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions:{
     deleteQuestion(question){
+      var answer_deletions = question.get('answers').map(function(answer){
+      return answer.destroyRecord();
+    });
+    Ember.RSVP.all(answer_deletions).then(function(){
       return question.destroyRecord();
-
-      this.transitionTo('index');
-    },
+    })
+    this.transitionTo('index');
+  },
     saveTheAnswer(params){
       var newAnswer = this.store.createRecord('answer', params);
       console.log("the q:", params.question);
@@ -16,6 +20,15 @@ export default Ember.Route.extend({
         return question.save();
       });
       this.transitionTo('question', params.question);
+    },
+    deleteAnswer(answer){
+      console.log("qjs:", answer);
+      var question = answer.get('question');
+      console.log("qjsq", answer.question);
+      answer.destroyRecord().then(function(){
+        question.save();
+      });
+      this.transitionTo('question', question);
     }
   }
 });

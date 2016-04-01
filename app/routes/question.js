ed595d954ae6de2ddcb7394e7a977fe253cc40e2
nic <netzeln@gmail.com>
2016-04-01 10:18:17 -0700

    add partially working delete question function

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions:{
+    deleteQuestion(question){
+      return question.destroyRecord();
+
+      this.transitionTo('index');
+    }
+  }
 });
